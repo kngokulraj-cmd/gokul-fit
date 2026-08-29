5301950e1bcde81f3e9351708c842213179e2a4b
kngokulraj-cmd <kngokulraj@gmail.com>
2026-08-29 04:43:05 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C="gokul-fit-v3";const A=["./","./index.html","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
